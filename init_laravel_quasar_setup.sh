536a529111a4fbc3e5b5ae0038e3fea5f91834cd
Chan Kuan Leang <chankl78@gmail.com>
2020-06-17 16:30:13 +0800

    20200617 Quasar Setup Changes

--- init_laravel_quasar_setup.sh
+++ init_laravel_quasar_setup.sh
@@ -2,18 +2,13 @@
 
 cd /var/www/html
 
-git clone https://gitlab.com/chankl78/laravel5.8-quasar1.0.git .
-
 composer update
 
 cp .env.example .env
 php artisan key:generate
 
-cd /var/www/html/src-spa/
+php artisan key:generate
 
-npm install
-npm install -g @quasar/cli
-npm install -g @vue/cli
-
-quasar c
-quasar b
+yarn global add @quasar/cli
+yarn global add @quasar/icongenie
+yarn global add @vue/cli
